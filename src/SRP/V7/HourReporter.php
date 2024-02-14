331d8b905287330bb94d1d5e4795ec6cba88c725
kutlumbek <Moralek228@gmail.com>
2024-02-14 18:39:51 +0300

    Добавил примеры SRP из книги чистый код

---
+++ src/SRP/V7/HourReporter.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\SRP\V7;
+
+class HourReporter
+{
+    public function reportHours() {}
+}
---
+++ src/SRP/V7/PayCalculator.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace App\SRP\V7;
+
+class PayCalculator
+{
+    public function calculatePay()
+    {}
+}
